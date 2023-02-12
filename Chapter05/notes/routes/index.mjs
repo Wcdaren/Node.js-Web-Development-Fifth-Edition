ee7daa55171f05b9116a09fdffa0b8b8f3a6e65e
wcdaren <wcdaren@gmail.com>
2023-02-12 09:30:40 +0000

    temp

--- Chapter05/notes/routes/index.mjs
+++ Chapter05/notes/routes/index.mjs
@@ -1,4 +1,4 @@
-// import * as util from 'util';
+import * as util from 'util';
 import { default as express } from 'express';
 import { NotesStore as notes } from '../app.mjs';
 export const router = express.Router();
@@ -7,12 +7,12 @@ export const router = express.Router();
 router.get('/', async (req, res, next) => {
     try {
         const keylist = await notes.keylist();
-        // console.log(`keylist ${util.inspect(keylist)}`);
+        console.log(`keylist ${util.inspect(keylist)}`);
         const keyPromises = keylist.map(key => {
             return notes.read(key);
         });
         const notelist = await Promise.all(keyPromises);
-        // console.log(util.inspect(notelist));
+        console.log(util.inspect(notelist));
         res.render('index', { title: 'Notes', notelist: notelist });
     } catch (err) {
         next(err);
